0a0cd126b4a5d37afe3aa4fa67d1383ea72d1701
PRAVEENRAJ <PRAVEENRAJ@DESKTOP-GE5H9EN>
2020-02-12 18:33:55 +0530

    Add project files.

---
+++ Web/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "Priya",
+                url: "{end}/{Action}/{id}",
+                defaults: new { controller = "Display", action = "End", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{Action}/{id}",
+                defaults: new { controller = "Events", action = "Start", id = UrlParameter.Optional }
+            );
+
+            
+        }
+    }
+}
